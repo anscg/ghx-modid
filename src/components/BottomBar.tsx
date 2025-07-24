30cfd3aa32063b16cb615b65a1de22a7c006b29a
Anson Chung <ansonchung929@outlook.com>
2025-07-24 20:50:06 +0800

    test

--- src/components/BottomBar.tsx
+++ src/components/BottomBar.tsx
@@ -103,7 +103,7 @@ const BottomBar: React.FC = () => {
         left: 0,
         right: 0,
         // Increased bottom padding to give the taller bar more space, plus safe area inset for mobile browsers
-        padding: `0 20px calc(34px + env(safe-area-inset-bottom)) 20px`,
+        padding: `0 20px calc(34px + env(safe-area-inset-bottom, 100px)) 20px`,
         zIndex: 105,
         display: "flex",
         alignItems: "center",
